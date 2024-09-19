5bef2b58a245d76aec0d1b23a2f5a49827dc7197
RUFES BELENGUER <daniel.rufes@gmail.com>
2024-09-19 17:48:02 +0200

    Creada carpeta y primer archivo

---
+++ Proyectos/Ejercicios1/info_basica.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenida</title>
+</head>
+<body>
+    <h1>Ejercicio 1</h1>
+    <?php
+    $año = 2005;
+    $nombre = "Daniel Rufes";
+    ?>
+    <p>Me llamo <?php print $nombre ?> y nací en el año <?php print $año ?>
+</body>
+</html>
