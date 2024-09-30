9c6369c304520e1556a1f7da196551a9c9436091
RUFES BELENGUER <daniel.rufes@gmail.com>
2024-09-30 16:43:48 +0200

    Ejercicios añadidos

---
+++ Proyectos/EjerciciosFunciones/Ej7.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    date_default_timezone_set('Europe/Madrid');
+    echo date("d/m/Y");
+    ?>
+</body>
+</html>
---
+++ Proyectos/EjerciciosFunciones/Ej7Ampl.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    date_default_timezone_set('Europe/Madrid');
+    $date = strtotime("09/03/2005");
+    $now = strtotime("10/03/2005");
+    echo $date
+    ?>
+</body>
+</html>
