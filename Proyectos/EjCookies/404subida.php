0a25fe7d5b54bd3c92f9c61f3b18301c16552eab
Daniel <daniel.rufes@gmail.com>
2024-11-06 16:16:18 +0100

    Videoclub añadido

--- Proyectos/EjCookies/404subida.php
+++ Proyectos/EjCookies/404subida.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
 <body>
-  <?php
-  if (isset($_POST['file'])) {
-    if (is_uploaded_file($_FILES['file']['tmp_name'])) {
-        echo "<p>Archivo $nombre subido con éxito</p>";
+    <?php
+    if (isset($_POST['btnSubir']) && $_POST['btnSubir'] == 'Subir') {
+        if (isset($_FILES['archivoEnviado']) && is_uploaded_file($_FILES['archivoEnviado']['tmp_name'])) {
+            $nombre = $_FILES['archivoEnviado']['name'];
+        } else {
+            echo "<p>No se ha subido ningún archivo</p>";
+        }
     }
-} else {echo "Ningún archivo subido";}
-  ?>
+    ?>
 </body>
-
 </html>
